bda17424181331eed17e6130f968fee157439f7f
nora-sch <nora.schumann.development@gmail.com>
2022-06-22 14:33:26 +0200

    Demo Mise en place d'une page de recherche / formulaire, list

--- src/app/rechercher/rechercher.component.ts
+++ src/app/rechercher/rechercher.component.ts
@@ -6,16 +6,50 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ['./rechercher.component.scss']
 })
 export class RechercherComponent implements OnInit{
-    public binding = 'Hello World!';
+public title = 'titre par défaut';
+public year: number;
+public type = '';
+public error = '';
+public films = [];
+
     constructor(){
 
     }
     ngOnInit(): void {
         
     }
-    clicBouton(){
-        console.log('Clic bouton!');
-        this.binding = 'Clic !'
+    rechercher(){
+      this.error = '';
+      if(!this.title || this.title.length<3){
+          this.error = 'Veuillez saisir un title de plus de trois caractères';
+      }
+      if(this.year && (this.year<1900 || this.year>2022)){
+        this.error = 'Veuillez saisir une année entre 1900 et 2022';
+        }
+        if(this.type === undefined){
+            this.error = 'Veuillez saisir un type de média';
+            }
+            //Fin des contrôles de surface
+            this.lancerRecherche();
+    }
 
+    private lancerRecherche(){
+        this.films = [
+            {
+                Title: 'Film A',
+                Year: 1952,
+                Poster: 'assets/icon/favicon.png'
+            },
+            {
+                Title: 'Film B',
+                Year: 1977,
+                Poster: 'assets/icon/favicon.png'
+            },
+            {
+                Title: 'Film C',
+                Year: 2012,
+                Poster: 'assets/icon/favicon.png'
+            }
+         ]
     }
 }
